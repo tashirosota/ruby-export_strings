f9e726ddb8e1de940239e9eae1b970dd751962fc
Tashiro <g4160hc@gmail.com>
2019-05-28 08:36:30 +0900

    first commit

---
+++ lib/export_strings.rb
@@ -0,0 +1,6 @@
+require "export_strings/version"
+
+module ExportStrings
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/export_strings/version.rb
@@ -0,0 +1,3 @@
+module ExportStrings
+  VERSION = "0.1.0"
+end
